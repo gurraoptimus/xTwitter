ea855afd0c81f5067ae8d6cd8b7300377f4c631c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2025-03-21 00:32:04 +0100

    xTwitter

--- xPy.py
+++ xPy.py
@@ -120,7 +120,7 @@ logo_position = ((qr_width - logo_size) // 2, (qr_height - logo_size) // 2)
 final_qr.paste(logo, logo_position, mask=logo)
 
 # Save the final QR code
-final_qr.save("instaPy.png")
+final_qr.save("xPy.png")
 
-print("✅ QR code with circular X logo and user image saved as 'instaPy'.")
+print("✅ QR code with circular X logo and user image saved as 'xPy'.")
 final_qr.show()  # Display the QR code
